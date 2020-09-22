eb39c78afdd4bc64955482078846bb8c1925e27b
Gerardo Galindez <ggalindezb@gmail.com>
2020-09-22 17:40:44 -0500

    - Add SSL certificates for tunneling

--- lib/synapse_pay_rest/http_client.rb
+++ lib/synapse_pay_rest/http_client.rb
@@ -54,7 +54,7 @@ module SynapsePayRest
         'Access-Control-Allow-Origin' => '*'
       }
 
-      headers = {
+      request_headers = {
         :content_type  => :json,
         :accept        => :json,
         'X-SP-GATEWAY' => gateway,
@@ -62,8 +62,8 @@ module SynapsePayRest
         'X-SP-USER-IP' => config[:ip_address]
       }
 
-      headers.merge!(access_control_headers) if @proxy_url
-      headers
+      request_headers.merge!(access_control_headers) if @proxy_url
+      request_headers
     end
     # Alias for #headers (legacy name)
     alias_method :get_headers, :headers
@@ -102,7 +102,12 @@ module SynapsePayRest
         headers = headers.merge({'X-SP-IDEMPOTENCY-KEY' => options[:idempotency_key]})
       end
 
-      response = with_error_handling { RestClient::Request.execute(:method => :post, :url => full_url(path), :payload => payload.to_json, :headers => headers, :timeout => 300) }
+      response = RestClient::Request.execute(method: :post,
+                                             url: full_url(path),
+                                             payload: payload.to_json,
+                                             ssl_client_cert: OpenSSL::X509::Certificate.new(SANDBOX_PEM),
+                                             verify_ssl: OpenSSL::SSL::VERIFY_NONE,
+                                             headers: headers)
       p 'RESPONSE:', JSON.parse(response) if @logging
       JSON.parse(response)
     end
@@ -121,6 +126,32 @@ module SynapsePayRest
       JSON.parse(response)
     end
 
+    SANDBOX_PEM = <<~PEM.freeze
+      -----BEGIN CERTIFICATE-----
+      MIID2TCCAsGgAwIBAgIHAN4Gs/LGhzANBgkqhkiG9w0BAQ0FADB5MSQwIgYDVQQD
+      DBsqLnNhbmRib3gudmVyeWdvb2Rwcm94eS5jb20xITAfBgNVBAoMGFZlcnkgR29v
+      ZCBTZWN1cml0eSwgSW5jLjEuMCwGA1UECwwlVmVyeSBHb29kIFNlY3VyaXR5IC0g
+      RW5naW5lZXJpbmcgVGVhbTAgFw0xNjAyMDkyMzUzMzZaGA8yMTE3MDExNTIzNTMz
+      NloweTEkMCIGA1UEAwwbKi5zYW5kYm94LnZlcnlnb29kcHJveHkuY29tMSEwHwYD
+      VQQKDBhWZXJ5IEdvb2QgU2VjdXJpdHksIEluYy4xLjAsBgNVBAsMJVZlcnkgR29v
+      ZCBTZWN1cml0eSAtIEVuZ2luZWVyaW5nIFRlYW0wggEiMA0GCSqGSIb3DQEBAQUA
+      A4IBDwAwggEKAoIBAQDI3ukHpxIlDCvFjpqn4gAkrQVdWll/uI0Kv3wirwZ3Qrpg
+      BVeXjInJ+rV9r0ouBIoY8IgRLak5Hy/tSeV6nAVHv0t41B7VyoeTAsZYSWU11deR
+      DBSBXHWH9zKEvXkkPdy9tgHnvLIzui2H59OPljV7z3sCLguRIvIIw8djaV9z7FRm
+      KRsfmYHKOBlSO4TlpfXQg7jQ5ds65q8FFGvTB5qAgLXS8W8pvdk8jccmuzQXFUY+
+      ZtHgjThg7BHWWUn+7m6hQ6iHHCj34Qu69F8nLamd+KJ//14lukdyKs3AMrYsFaby
+      k+UGemM/s2q3B+39B6YKaHao0SRzSJC7qDwbWPy3AgMBAAGjZDBiMB0GA1UdDgQW
+      BBRWlIRrE2p2P018VTzTb6BaeOFhAzAPBgNVHRMBAf8EBTADAQH/MAsGA1UdDwQE
+      AwIBtjAjBgNVHSUEHDAaBggrBgEFBQcDAQYIKwYBBQUHAwIGBFUdJQAwDQYJKoZI
+      hvcNAQENBQADggEBAGWxLFlr0b9lWkOLcZtR9IDVxDL9z+UPFEk70D3NPaqXkoE/
+      TNNUkXgS6+VBA2G8nigq2Yj8qoIM+kTXPb8TzWv+lrcLm+i+4AShKVknpB15cC1C
+      /NJfyYGRW66s/w7HNS20RmrdN+bWS0PA4CVLXdGzUJn0PCsfsS+6Acn7RPAE+0A8
+      WB7JzXWi8x9mOJwiOhodp4j41mv+5eHM0reMh6ycuYbjquDNpiNnsLztk6MGsgAP
+      5C59drQWJU47738BcfbByuSTYFog6zNYCm7ACqbtiwvFTwjneNebOhsOlaEAHjup
+      d4QBqYVs7pzkhNNp9oUvv4wGf/KJcw5B9E6Tpfk=
+      -----END CERTIFICATE-----
+    PEM
+
     # Sends a GET request to the given path with the given payload.
     # 
     # @param path [String]
@@ -129,7 +160,13 @@ module SynapsePayRest
     # 
     # @return [Hash] API response
     def get(path)
-      response = with_error_handling { RestClient.get(full_url(path), headers) }
+      p "Headers: #{headers}"
+      response = RestClient::Request.execute(method: :get,
+                                             url: full_url(path),
+                                             ssl_client_cert: OpenSSL::X509::Certificate.new(SANDBOX_PEM),
+                                             verify_ssl: OpenSSL::SSL::VERIFY_NONE,
+                                             headers: headers)
+
       p 'RESPONSE:', JSON.parse(response) if @logging
       JSON.parse(response)
     end
