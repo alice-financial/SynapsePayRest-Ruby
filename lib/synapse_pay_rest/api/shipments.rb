6638c04e61326c5a9f21074ec8e4fd236222574d
Tonatiuh Núñez <tona@thisisalice.com>
2021-05-12 13:29:57 -0500

    add code in SynapsePayRest::Shipment and SynapsePayRest::Client to handle shipment creation from there

--- lib/synapse_pay_rest/api/shipments.rb
+++ lib/synapse_pay_rest/api/shipments.rb
@@ -25,17 +25,39 @@ module SynapsePayRest
     # @return [Hash] API response
     #
     def get(user_id:, node_id:, subnet_id:, id:)
-      path = shipment_resource_path(user_id: user_id,
-                                    node_id: node_id,
-                                    subnet_id: subnet_id,
-                                    id: id)
+      path = shipments_resource_path(user_id: user_id,
+                                     node_id: node_id,
+                                     subnet_id: subnet_id,
+                                     id: id)
       client.get(path)
+    end
+
+    # Sends a POST request to /shipments endpoint to create a new shipment.
+    # Returns the response.
+    #
+    # @param user_id [String] user_id associated with the subnet
+    # @param node_id [String] node the subnet belongs to
+    # @param subnet_id [String] subnet to which the shipment belongs to
+    # @param payload [Hash]
+    # @see https://docs.synapsefi.com/api-references/shipments/shipment-object-details payload structure
+    #
+    # @raise [SynapsePayRest::Error] may return subclasses of error based on
+    # HTTP response from API
+    #
+    # @return [Hash] API response
+    def create(user_id:, node_id:, subnet_id:, payload:)
+      path = shipments_resource_path(user_id: user_id,
+                                     node_id: node_id,
+                                     subnet_id: subnet_id)
+      client.post(path, payload)
     end
 
     private
 
-    def shipment_resource_path(user_id:, node_id:, subnet_id:, id:)
-      "/users/#{user_id}/nodes/#{node_id}/subnets/#{subnet_id}/ship/#{id}"
+    def shipments_resource_path(user_id:, node_id:, subnet_id:, id: nil)
+      path = "/users/#{user_id}/nodes/#{node_id}/subnets/#{subnet_id}/ship"
+      path += "/#{id}" if id
+      path
     end
   end
 end
