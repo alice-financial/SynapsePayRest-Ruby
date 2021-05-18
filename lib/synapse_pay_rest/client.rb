566f1c4e4f4e38a2cc82f9281006cf4bd6f4d935
Tonatiuh Núñez <tonatiuhnb@gmail.com>
2021-05-18 15:05:09 -0500

    Revert "[ENG-5682] Add standalone SynapsePayRest::Shipment model (#22)"
    This reverts commit 37be0438835251abb8ee29c15293e01180a9cb16.

--- lib/synapse_pay_rest/client.rb
+++ lib/synapse_pay_rest/client.rb
@@ -13,8 +13,7 @@ module SynapsePayRest
     #   @return [SynapsePayRest::Transactions]
     # @!attribute [rw] subscriptions
     #   @return [SynapsePayRest::Subscriptions]
-    attr_accessor :http_client, :users, :nodes, :subnets, :shipments,
-                  :transactions, :subscriptions, :institutions,
+    attr_accessor :http_client, :users, :nodes, :subnets, :transactions, :subscriptions, :institutions,
                   :client_endpoint, :atms, :crypto_quotes, :statements
 
     # Alias for #transactions (legacy name)
@@ -45,7 +44,6 @@ module SynapsePayRest
       @users            = Users.new @http_client
       @nodes            = Nodes.new @http_client
       @subnets          = Subnets.new @http_client
-      @shipments        = Shipments.new @http_client
       @transactions     = Transactions.new @http_client
       @subscriptions    = Subscriptions.new @http_client
       @institutions     = Institutions.new @http_client
